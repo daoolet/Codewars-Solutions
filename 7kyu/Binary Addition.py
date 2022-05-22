799cf2781c359e1330285568c62ba6e7b71ea81f
Daulet <dauletkhabidullin@gmail.com>
2022-05-22 14:56:24 +0500

    Solved, but googled

--- 7kyu/Binary Addition.py
+++ 7kyu/Binary Addition.py
@@ -1,12 +1,2 @@
-a = 2
-b = 2
-res = a+b
-s = ''
-while res > 0:
-    if res%2:
-        print('1',end='')
-    else:
-        print('0',end='')
-    res = res// 2
-
-print(s)
+def add_binary(a,b):
+    return bin(a+b).replace('0b', '')
