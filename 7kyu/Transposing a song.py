6560b1474c32412caa67d6686c2ccb9aa98fefc6
Daulet <dauletkhabidullin@gmail.com>
2022-05-29 11:28:14 +0500

    Solved

---
+++ 7kyu/Transposing a song.py
@@ -0,0 +1,7 @@
+sharp = 'A,A#,B,C,C#,D,D#,E,F,F#,G,G#'.split(',')
+flat  = 'A,Bb,B,C,Db,D,Eb,E,F,Gb,G,Ab'.split(',')
+ans = []
+
+def transpose(song, interval):
+    return [sharp[(sharp.index(i)+interval)%12] if i in sharp else sharp[(flat.index(i)+interval)%12] for i in song]
+
