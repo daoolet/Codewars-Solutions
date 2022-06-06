4126ecc3efb55ba1261333dc1512ef22c66c07d3
Daulet <dauletkhabidullin@gmail.com>
2022-06-06 10:59:43 +0600

    Solved, but googled

---
+++ 7kyu/Two fighters, one winner.py
@@ -0,0 +1,16 @@
+def declare_winner(fighter1, fighter2, first_attacker):
+    turn = first_attacker
+
+    while True:
+        if turn == fighter1.name:
+            fighter2.health -= fighter1.damage_per_attack
+            turn = fighter2.name
+
+        elif turn == fighter2.name:
+            fighter1.health -= fighter2.damage_per_attack
+            turn = fighter1.name
+
+        if fighter2.health <= 0:
+            return fighter1.name
+        if fighter1.health <= 0:
+            return fighter2.name
