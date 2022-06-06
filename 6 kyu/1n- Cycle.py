d16068a2dfd96efce8c12eac0fdaed1eb746ff27
Daulet <dauletkhabidullin@gmail.com>
2022-06-06 12:47:55 +0600

    Solved, but googled

---
+++ 6 kyu/1n- Cycle.py
@@ -0,0 +1,10 @@
+def cycle(n):
+    if (n%2 == 0 or n%5 == 0):
+        return -1
+    else:
+        res = 10%n
+        c = 1
+        while res != 1:
+            res = res*10%n
+            c+=1
+        return c
